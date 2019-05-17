3f4e27f7f5e80ac435baf78aecff608da1de31b6
brickleq <pblequire@gmail.com>
2019-05-16 19:07:09 -0500

    part 1 now working

--- mission_to_mars2.py
+++ mission_to_mars2.py
@@ -11,14 +11,12 @@ import platform
 from bs4 import BeautifulSoup as bs
 from splinter import Browser
 from selenium import webdriver
-
 #%%
 # Set URL, run requests query, convert response to BeautifulSoup
 url = 'https://mars.nasa.gov/news/'
 response = requests.get(url)
 soup = bs(response.text,'lxml')
 print(soup.prettify())
-
 #%%
 # Get title and paragraph for first news article 
 title = soup.find('div',class_='content_title')
@@ -27,20 +25,18 @@ paragraph = soup.find('div',class_='rollover_description_inner')
 article_paragraph = paragraph.text
 print(article_title)
 print(article_paragraph)
-
 #%%
 # Use Splinter to navigate to JPL Featured Image 
 browser = Browser('chrome')
 url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 browser.visit(url)
-time.sleep(2)
+time.sleep(5)
 browser.find_by_id('full_image').click()
-time.sleep(2)
+time.sleep(5)
 browser.find_link_by_partial_href('spaceimages/details.php?').click()
-time.sleep(2)
+time.sleep(5)
 browser.find_link_by_partial_href('hires.jpg').click()
 featured_image_url = browser.url
-
 #%%
 ### Mars Facts
 # Visit the Mars Facts webpage [here](https://space-facts.com/mars/) and use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
@@ -55,37 +51,53 @@ print(html_string)
 html_table_string = pd.read_html(str(html_string))
 html_table_string
 #%%
-
-
-
-
-
-
-
-'''
 ### Mars Hemispheres
 
-* Visit the USGS Astrogeology site [here](https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars) to obtain high resolution images for each of Mar's hemispheres.
+# Visit the USGS Astrogeology site [here](https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars) to obtain high resolution images for each of Mar's hemispheres.
+url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+browser.visit(url)
+hemispheres = ['Cerberus Hemisphere','Schiaparelli Hemisphere','Syrtis Major Hemisphere','Valles Marineris Hemisphere']
+hemisphere_images = []
+i = 0
+for hemisphere in hemispheres:
+    time.sleep(5)
+    browser.find_link_by_partial_text(' Hemisphere')[i].click()
+    time.sleep(5)
+    browser.find_link_by_text("Original").click()
+    time.sleep(15)
+    url = browser.url
+    dict = {'title':hemisphere,'img_url':url}
+    hemisphere_images.append(dict)
+    browser.back()
+    time.sleep(5)
+    i+=1
+hemisphere_images
+#%%
 
-* You will need to click each of the links to the hemispheres in order to find the image url to the full resolution image.
 
-* Save both the image url string for the full resolution hemisphere image, and the Hemisphere title containing the hemisphere name. Use a Python dictionary to store the data using the keys `img_url` and `title`.
+#%%
 
-* Append the dictionary with the image url string and the hemisphere title to a list. This list will contain one dictionary for each hemisphere.
+# You will need to click each of the links to the hemispheres in order to find the image url to the full resolution image.
 
-```python
+# Save both the image url string for the full resolution hemisphere image, and the Hemisphere title containing the hemisphere name. Use a Python dictionary to store the data using the keys `img_url` and `title`.
+
+# Append the dictionary with the image url string and the hemisphere title to a list. This list will contain one dictionary for each hemisphere.
+
+#```python
 # Example:
-hemisphere_image_urls = [
-    {"title": "Valles Marineris Hemisphere", "img_url": "..."},
-    {"title": "Cerberus Hemisphere", "img_url": "..."},
-    {"title": "Schiaparelli Hemisphere", "img_url": "..."},
-    {"title": "Syrtis Major Hemisphere", "img_url": "..."},
-]
-```
+#hemisphere_image_urls = [
+#    {"title": "Valles Marineris Hemisphere", "img_url": "..."},
+#    {"title": "Cerberus Hemisphere", "img_url": "..."},
+#    {"title": "Schiaparelli Hemisphere", "img_url": "..."},
+#    {"title": "Syrtis Major Hemisphere", "img_url": "..."},
+#]
+#```
 
-- - -
+#- - -
 
+#%%
 ## Step 2 - MongoDB and Flask Application
+'''
 
 Use MongoDB with Flask templating to create a new HTML page that displays all of the information that was scraped from the URLs above.
 
@@ -129,5 +141,3 @@ To submit your work to BootCampSpot, create a new GitHub repository and upload t
 
 
 '''
-
-#%%

c97fbd93cf28ba79c61099283668a6cd3927f519
brickleq <pblequire@gmail.com>
2019-05-18 15:42:15 -0500

    in media res

--- scrape_mars.py
+++ scrape_mars.py
@@ -14,121 +14,111 @@ Use MongoDB with Flask templating to create a new HTML page that displays all of
 get_ipython().system('[final_app_part1.png](Images/final_app_part1.png)')
 get_ipython().system('[final_app_part2.png](Images/final_app_part2.png)')
 '''
+
+### Use MongoDB with Flask templating to create a new HTML page that displays all of the information that was scraped from the URLs above.
+
 #%%
 # Dependencies
 import pandas as pd
-import requests
 import pymongo
+import requests
 import splinter
 import selenium
 import time
 import platform
 from bs4 import BeautifulSoup as bs
-from splinter import Browser
+from flask import Flask
 from selenium import webdriver
-#%%
-def scrape_Mars():
-
-dict = {}
-dict['website'] = [] 
-    return dict
-#%%
-# Set URL, run requests query, convert response to BeautifulSoup
-url = 'https://mars.nasa.gov/news/'
-response = requests.get(url)
-soup = bs(response.text,'lxml')
-#print(soup.prettify())
-#%%
-# Get title and paragraph for first news article 
-title = soup.find('div',class_='content_title')
-article_title = title.text
-paragraph = soup.find('div',class_='rollover_description_inner')
-article_paragraph = paragraph.text
-#print(article_title)
-#print(article_paragraph)
-#%%
-# Use Splinter to navigate to JPL Featured Image 
-browser = Browser('chrome')
-
-url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-browser.visit(url)
-time.sleep(5)
-browser.find_by_id('full_image').click()
-time.sleep(5)
-browser.find_link_by_partial_href('spaceimages/details.php?').click()
-time.sleep(5)
-browser.find_link_by_partial_href('hires.jpg').click()
-featured_image_url = browser.url
-#%%
+from splinter import Browser
 
 #%%
-### Mars Facts
-# Visit the Mars Facts webpage [here](https://space-facts.com/mars/) and use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
-url = 'https://space-facts.com/mars/'
-response = requests.get(url)
-soup = bs(response.text,'html.parser')
-#print(soup.prettify())
+# Start by converting your Jupyter notebook into a Python script called `scrape_mars.py` with a function called `scrape` that will execute all of your scraping code from above and return one Python dictionary containing all of the scraped data.
+
+def scrape():
+
+    # Set URL, run requests query, convert response to BeautifulSoup
+    url = 'https://mars.nasa.gov/news/'
+    response = requests.get(url)
+    soup = bs(response.text,'lxml')
+
+    # Get title and paragraph for first news article 
+    title = soup.find('div',class_='content_title')
+    article_title = title.text
+    paragraph = soup.find('div',class_='rollover_description_inner')
+    article_paragraph = paragraph.text
+
+    # Use Splinter to navigate to JPL Featured Image 
+    browser = Browser('chrome')
+
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+    time.sleep(2)
+    browser.find_by_id('full_image').click()
+    time.sleep(2)
+    browser.find_link_by_partial_href('spaceimages/details.php?').click()
+    time.sleep(2)
+    browser.find_link_by_partial_href('hires.jpg').click()
+    featured_image_url = browser.url
+
+    ### Mars Facts
+    # Visit the Mars Facts webpage [here](https://space-facts.com/mars/) and use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
+    url = 'https://space-facts.com/mars/'
+    response = requests.get(url)
+    soup = bs(response.text,'html.parser')
+
+    # Use Pandas to convert the data to a HTML table string.
+    html_string = soup.find('table')
+    html_table_string = pd.read_html(str(html_string))
+
+    ### Mars Hemispheres
+    # Visit the USGS Astrogeology site [here](https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars) to obtain high resolution images for each of Mar's hemispheres.
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+    hemispheres = ['Cerberus Hemisphere','Schiaparelli Hemisphere','Syrtis Major Hemisphere','Valles Marineris Hemisphere']
+    hemisphere_images = []
+
+    i = 0
+    for hemisphere in hemispheres:
+        # You will need to click each of the links to the hemispheres in order to find the image url to the full resolution image.
+        browser.find_link_by_partial_text(' Hemisphere')[i].click(); time.sleep(2)
+        browser.find_link_by_text("Original").click();time.sleep(2)
+        # Save both the image url string for the full resolution hemisphere image, and the Hemisphere title containing the hemisphere name. Use a Python dictionary to store the data using the keys `img_url` and `title`.
+        url = browser.url
+        # Append the dictionary with the image url string and the hemisphere title to a list. This list will contain one dictionary for each hemisphere.
+        hemisphere_dict = {'url':url,'title':hemisphere}
+        hemisphere_images.append(hemisphere_dict)
+        browser.back()
+        time.sleep(2)
+        i+=1
+
+    return hemisphere_images
+hemisphere_images = scrape()
+print(hemisphere_images)
+
 #%%
-# Use Pandas to convert the data to a HTML table string.
-html_string = soup.find('table')
-#print(html_string)
-html_table_string = pd.read_html(str(html_string))
-html_table_string
-#%%
-### Mars Hemispheres
+#* Next, create a route called `/scrape` that will import your `scrape_mars.py` script and call your `scrape` function.
 
-# Visit the USGS Astrogeology site [here](https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars) to obtain high resolution images for each of Mar's hemispheres.
-url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-browser.visit(url)
-# You will need to click each of the links to the hemispheres in order to find the image url to the full resolution image.
-hemispheres = ['Cerberus Hemisphere','Schiaparelli Hemisphere','Syrtis Major Hemisphere','Valles Marineris Hemisphere']
-hemisphere_images = []
-i = 0
-for hemisphere in hemispheres:
-    browser.find_link_by_partial_text(' Hemisphere')[i].click(); time.sleep(5)
-    browser.find_link_by_text("Original").click();time.sleep(5)
-    url = browser.url
-    dict = {'title':hemisphere,'img_url':url}
-    hemisphere_images.append(dict)
-    browser.back()
-    time.sleep(5)
-    i+=1
-hemisphere_images
-#%%
+app = Flask(__name__)
 
-# * Start by converting your Jupyter notebook into a Python script called `scrape_mars.py` with a function called `scrape` that will execute all of your scraping code from above and return one Python dictionary containing all of the scraped data.
-
-def scrape(params):
+@app.route("/scrape")
+def run_scraper():
+    print("Server received request for '/scrape' page...")
+    return "Welcome to my 'Home' page!"
 
 
-#%%
+# 4. Define what to do when a user hits the /about route
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
 
-# You will need to click each of the links to the hemispheres in order to find the image url to the full resolution image.
 
-# Save both the image url string for the full resolution hemisphere image, and the Hemisphere title containing the hemisphere name. Use a Python dictionary to store the data using the keys `img_url` and `title`.
+if __name__ == "__main__":
+    app.run(debug=True)
 
-# Append the dictionary with the image url string and the hemisphere title to a list. This list will contain one dictionary for each hemisphere.
 
-#```python
-# Example:
-#hemisphere_image_urls = [
-#    {"title": "Valles Marineris Hemisphere", "img_url": "..."},
-#    {"title": "Cerberus Hemisphere", "img_url": "..."},
-#    {"title": "Schiaparelli Hemisphere", "img_url": "..."},
-#    {"title": "Syrtis Major Hemisphere", "img_url": "..."},
-#]
-#```
 
-#- - -
-
-#%%
-## Step 2 - MongoDB and Flask Application
 '''
-
-
-
-- - -
-
 ## Step 3 - Submission
 
 To submit your work to BootCampSpot, create a new GitHub repository and upload the following:
@@ -150,7 +140,4 @@ To submit your work to BootCampSpot, create a new GitHub repository and upload t
 ## Copyright
 
 © 2019 Trilogy Education Services. All Rights Reserved.
-
-
-
 '''
